6202b362dbac22e6f20fff850f914dfb38afa895
luuphuphat69 <luuphuphat69@gmail.com>
2025-08-18 22:00:43 +0700

    first commit

---
+++ api request/openweather.ts
@@ -0,0 +1,17 @@
+async function makeCurrentWeatherRequest<T>(url: string): Promise<T | null> {
+  const headers = {
+    "User-Agent": "weather-app/1.0",
+    Accept: "*/*",
+  };
+
+  try {
+    const response = await fetch(url, { headers });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return (await response.json()) as T;
+  } catch (error) {
+    console.error("Error making NWS request:", error);
+    return null;
+  }
+}
